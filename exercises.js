c5d0d95933dbdbae268109fe70b1bbec8329bbfe
Thebeleiver1 <punyachapagain@gmail.com>
2017-03-30 21:05:26 +1100

    hdfhda

--- exercises.js
+++ exercises.js
@@ -7,61 +7,103 @@ function makeCat(name, age) {
   //add an age property to the object with the value set to the age argument
   //add a method called meow that returns the string 'Meow!'
   //return the object
-}
+  var animal = {
+    name: name,
+    age: age,
+    meow: function(){
+      return 'Meow!';
+    }
+    
+  };
+  return animal;
+  }
+
 
 function addProperty(object, property) {
   //add the property to the object with a value of null
   //return the object
   //note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+  object[property] = null;
+
+  return object;
 }
 
 function invokeMethod(object, method) {
   //method is a string that contains the name of a method on the object
   //invoke this method
   //nothing needs to be returned
+  object[method]();
 }
 
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
   //mysteryNumberObject has a property called mysteryNumber
   //multiply the mysteryNumber property by 5 and return the product
+  var num = mysteryNumberObject.mysteryNumber * 5;
+  return num;
 }
 
 function deleteProperty(object, property) {
   //remove the property from the object
   //return the object
+  delete object[property];
+  return object;
 }
 
 function newUser(name, email, password) {
   //create a new object with properties matching the arguments passed in.
   //return the new object
+  var person = {
+    name: name,
+    email: email,
+    password: password
+  };
+
+  return person;
 }
 
 function hasEmail(user) {
   //return true if the user has a value for the property 'email'
   //otherwise return false
+  if (user.email !== undefined && user.email.length > 0 )
+  {
+    return true;
+  }
+  return false;
 }
 
 function hasProperty(object, property) {
   //return true if the object has the value of the property argument
   //property is a string
   //otherwise return false
+  if(object[property] !== undefined && object[property] === true){
+    return true;
+  }
+  return false;
 }
 
 function verifyPassword(user, password) {
   //check to see if the provided password matches the password property on the user object
   //return true if they match
   //otherwise return false
+  if(user.password === password){
+    return true;
+  }
+  return false;
 }
 
 function updatePassword(user, newPassword) {
   //replace the existing password on the user object with the value of newPassword
   //return the object
+  user.password = newPassword;
+  return user;
 }
 
 function addFriend(user, newFriend) {
   //user has a property called friends that is an array
   //add newFriend to the end of the friends array
   //return the user object
+  user.friends.push(newFriend);
+  return user;
 }
 
 function setUsersToPremium(users) {
